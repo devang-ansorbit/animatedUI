2f4d35cc7b46017f6ddcb533070ba06cf3f875fd
devang-ansorbit <devang.patel@ansorbit.com>
2022-11-17 18:16:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./animation.css";
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <div className="animation1"></div> */}
+      <div className='hoverEffect'></div>
+    </div>
+  );
+}
+
+export default App;
